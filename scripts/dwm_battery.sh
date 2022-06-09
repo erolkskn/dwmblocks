582e602873239a46d33c2dde9369de2820a3bfd3
Erol Keskin <erolkeskin.dev@gmail.com>
2022-06-09 16:53:39 +0300

    fix: battery icons

--- scripts/dwm_battery.sh
+++ scripts/dwm_battery.sh
@@ -1,14 +1,26 @@
 #!/bin/sh
 
 BATTERY="BAT0"
-ICON=""
+ICON=""
+CAP=$(cat /sys/class/power_supply/$BATTERY/capacity)
+
+if [ "$CAP" -lt 25 ];
+then
+    ICON=""
+elif [ "$CAP" -lt 50 ];
+then
+    ICON=""
+elif [ "$CAP" -lt 90 ];
+then
+    ICON=""
+else
+    ICON=""
+fi
 
 case "$(cat /sys/class/power_supply/$BATTERY/status)" in
 	"Charging")
 			ICON=" $ICON"
 			;;
 	esac
-
-CAP=$(cat /sys/class/power_supply/$BATTERY/capacity)
 
 printf "%s  %s%%" "$ICON" "$CAP"
