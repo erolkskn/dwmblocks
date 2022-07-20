1f4f7b5c465fb60fee2fafac47e607b5d70f77f6
Erol Keskin <erolkeskin.dev@gmail.com>
2022-07-20 14:57:17 +0300

    fix: indentation in blocks.def.h

--- blocks.def.h
+++ blocks.def.h
@@ -1,6 +1,6 @@
 static const Block blocks[] = {
-    { "", "dwm_bluetooth.sh", 600, 1 },
-    { "", "dwm_wifi.sh", 30, 1 },
+    	{ "", "dwm_bluetooth.sh", 600, 1 },
+    	{ "", "dwm_wifi.sh", 30, 1 },
 	{ "", "dwm_sound.sh", 60, 1 },
 	{ "", "dwm_battery.sh", 30, 0 },
 	{ "", "dwm_memory.sh",	10,	0 },
